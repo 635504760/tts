ed9f3ac68e89af56f74be677fdb7735ae4c17401
635504760 <635504760@qq.com>
2017-03-03 16:53:32 +0800

    uyy

---
+++ dapperexample.Repository/CustomClassMapper.cs
@@ -0,0 +1,17 @@
+﻿using DapperExtensions.Mapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dapperexample.Repository
+{
+    public class CustomClassMapper<T>: AutoClassMapper<T> where T:class
+    {
+        public override void Table(string tableName)
+        {
+            base.Table(tableName);
+        }
+    }
+}
